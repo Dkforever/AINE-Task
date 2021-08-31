e94c43a0a2e5b0ff87d67f33d04577852bd6ead7
Dkforever <64226873+Dkforever@users.noreply.github.com>
2021-08-31 22:10:59 +0530

    Update post_upload.php

--- post_upload.php
+++ post_upload.php
@@ -11,7 +11,7 @@ $postion = mysql_real_escape_string($_POST['position']);
 $skill = mysql_real_escape_string($_POST['skill']);
 $salary = mysql_real_escape_string($_POST['salary']);
 $city = mysql_real_escape_string($_POST['city']);
-$jod_description = mysql_real_escape_string($_POST['job_description']);
+$job_description = mysql_real_escape_string($_POST['job_description']);
 $address = mysql_real_escape_string($_POST['address']);
 
 
